eefd235c178bc16ec033f3734aefeb030540aea2
minhthien309 <minhthien7@gmail.com>
2019-09-21 20:00:16 +0700

    Add files via upload

---
+++ precache-manifest.3a677b56e7a516aeedb6c2e1a9c6003c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "624fd0ca452bafc07b90",
+    "url": "/js/chunk-vendors.7a16b304.js"
+  },
+  {
+    "revision": "f52279472b526cb728d8",
+    "url": "/js/app.b9f17491.js"
+  },
+  {
+    "revision": "f4eab566d74871a3461a",
+    "url": "/js/about.7fbc36c4.js"
+  },
+  {
+    "revision": "2b718373d343d44533c41a1005528e44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f52279472b526cb728d8",
+    "url": "/css/app.cbd0c9e4.css"
+  }
+];
